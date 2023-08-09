d22a948f13ac02b23fff5b3997936ac1199ef9f8
Nitierk <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-08-09 16:00:49 -0300

    Cronômetro

---
+++ src/script.js
@@ -0,0 +1,39 @@
+var iniciar = document.getElementById('Iniciar');
+var pausar = document.getElementById('Pausar');
+var reiniciar = document.getElementById('Reiniciar');
+var cronometroElement = document.getElementById('Cronometro');
+var intervalId;
+var contador = 0;
+
+iniciar.addEventListener('click', () => {
+    if (!intervalId) {
+        intervalId = setInterval(function() {
+            contador += 1;
+            displayCronometro();
+        }, 10); // Update interval to 10 milliseconds for centiseconds
+    }
+});
+
+pausar.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+});
+
+reiniciar.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    contador = 0;
+    displayCronometro();
+});
+
+function displayCronometro() {
+    var centiseconds = contador % 100;
+    var seconds = Math.floor(contador / 100) % 60;
+    var minutes = Math.floor(contador / 6000);
+
+    var centisecondsDisplay = centiseconds.toString().padStart(2, '0');
+    var secondsDisplay = seconds.toString().padStart(2, '0');
+    var minutesDisplay = minutes.toString().padStart(2, '0');
+
+    cronometroElement.innerHTML = `${minutesDisplay}:${secondsDisplay}:${centisecondsDisplay}`;
+}
